ee7de1225c3260706b17b4fd8256d9d5a362656a
kamrulT30048 <kamrul.hasan@bjitacademy.com>
2022-11-22 16:50:55 +0600

    good coding practice

--- app/src/main/java/com/kamrulhasan/tiptime/MainActivity.kt
+++ app/src/main/java/com/kamrulhasan/tiptime/MainActivity.kt
@@ -1,76 +1,62 @@
 package com.kamrulhasan.tiptime
 
-import android.annotation.SuppressLint
-import android.graphics.Color
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import androidx.core.graphics.red
 import com.kamrulhasan.tiptime.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.emptyFlow
 import java.text.NumberFormat
-import kotlin.math.cos
-import kotlin.math.round
+import kotlin.math.ceil
 
-@SuppressLint("StaticFieldLeak")
-public lateinit var binding: ActivityMainBinding
-val TAG : String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val tag: String = "MainActivity"
+    private val mgs: String = "logcat"
 
+    // @Activity onCreate Function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //Initialisation binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         //initialisation
-        binding.tvResult.text = getString(R.string.text_result,"--")
-
-
-       // binding.btnCalculate.setBackgroundResource(R.drawable.border_radius)
-
+        binding.tvResult.text = getString(R.string.text_result, "--")
         // custom input visibility control
-        binding.rbtnOther.setOnCheckedChangeListener{
-                buttonView, isChecked ->
-            if(isChecked){
+        binding.rbtnOther.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
                 binding.etOtherRadiobutton.visibility = View.VISIBLE
-            }
-            else{
+            } else {
                 binding.etOtherRadiobutton.visibility = View.GONE
                 binding.etOtherRadiobutton.text = null
             }
         }
         // Calculate button activities
-        binding.btnCalculate.setOnClickListener(View.OnClickListener {
-
-            if(binding.etPrice.text.isEmpty()){
-                binding.etPrice.setError("Required!")
-                Log.e(TAG,"Cost amount must be fill up.")
-            }
-            else if (binding.etOtherRadiobutton.visibility == View.VISIBLE
-                && binding.etOtherRadiobutton.text.isEmpty()){
-
-                binding.etOtherRadiobutton.setError("Required")
-                Log.e(TAG,"Input percentage must be fill up.")
-            }
-            else{
+        binding.btnCalculate.setOnClickListener {
+            if (binding.etPrice.text.isEmpty()) {
+                binding.etPrice.error = "Required!"
+                Log.e(tag, "Cost amount must be fill up.")
+            } else if (binding.etOtherRadiobutton.visibility == View.VISIBLE
+                && binding.etOtherRadiobutton.text.isEmpty()
+            ) {
+                binding.etOtherRadiobutton.error = "Required"
+                Log.e(tag, "Input percentage must be fill up.")
+            } else {
                 tipCalculate()
             }
-
-        })
-
-        binding.btnReset.setOnClickListener(View.OnClickListener {
+        }
+        binding.btnReset.setOnClickListener {
             reset()
-        })
+        }
     }
-    fun tipCalculate(){
 
-        val tipOption = binding.rgOptionOfTip.checkedRadioButtonId
-        val tipPercentage = when(tipOption){
+    // calculation function
+    private fun tipCalculate() {
+        //cause for crash the app
+        //binding.etPrice.text = null
+        val tipPercentage = when (binding.rgOptionOfTip.checkedRadioButtonId) {
             R.id.rbtn_amazing -> 0.2
             R.id.rbtn_good -> 0.18
             R.id.rbtn_ok -> 0.15
@@ -78,25 +64,22 @@ class MainActivity : AppCompatActivity() {
                 .toString()
                 .toDouble() / 100.0
         }
-
         var cost = binding.etPrice.text.toString().toDouble() * tipPercentage
-
-        if (binding.swtRound.isChecked){
+        if (binding.swtRound.isChecked) {
             cost = kotlin.math.ceil(cost)
-            Log.d(TAG,"Round is checked.")
+            Log.d(tag, "Round is checked.")
         }
         val tipCurrency = NumberFormat.getCurrencyInstance().format(cost)
-        binding.tvResult.text = getString(R.string.text_result,tipCurrency)
-
+        binding.tvResult.text = getString(R.string.text_result, tipCurrency)
     }
 
     //reset all values
-    fun reset(){
+    private fun reset() {
         binding.etPrice.text = null
+//        binding.etPrice.error = View.VISIBLE
         binding.etOtherRadiobutton.text = null
         binding.rgOptionOfTip.check(R.id.rbtn_amazing)
         binding.tvResult.text = getString(R.string.text_result, "--")
-
+        binding.swtRound.isChecked = true
     }
-
 }
